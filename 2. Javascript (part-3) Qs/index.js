cdc6ace58b7f92ed500cfa5835ac9a4361b27f8e
Ratnesh Kumar <154334703+ratnesh-45@users.noreply.github.com>
2024-06-24 11:35:23 +0530

    Add files via upload

---
+++ 2. Javascript (part-3) Qs/index.js
@@ -0,0 +1,18 @@
+//Qs.1:-
+
+// let msg=" help!";
+// let nweMsg=msg.trim().toUpperCase();
+// alert(nweMsg);
+
+//Qs.2:-
+
+// let name="ApnaCollege";
+// console.log(name.slice(4,9));
+// console.log(name.indexOf("na"));
+// console.log(name.replace("Apna","Our"));
+
+//Qs.3:-
+
+let name="ApnaCollege";
+alert(name.replace('l','t'));
+
