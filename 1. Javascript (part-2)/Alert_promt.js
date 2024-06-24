e10e0557e99727962bca68cb8c1ceb6685bbf7b2
Ratnesh Kumar <154334703+ratnesh-45@users.noreply.github.com>
2024-06-24 11:30:56 +0530

    Add files via upload

---
+++ 1. Javascript (part-2)/Alert_promt.js
@@ -0,0 +1,14 @@
+//print Alert:-
+// alert(" Welcome Coder!");
+
+
+// let firstName=alert("ratnesh");
+// console.log(firstName);
+
+//print Promt:-
+let firstName=prompt("Enter the first name:-");
+let lastName=prompt("Enter the last name:-");
+let fullName=firstName+" "+lastName;
+// console.log(fullName);
+alert(fullName);
+
---
+++ 1. Javascript (part-2)/first.js
@@ -0,0 +1,139 @@
+// console.log("Hello coder!");
+// console.log("I am a Coder.");
+// let a=b=5;
+// let sum=a+b;
+// console.log("sum:-"+sum);
+
+// let pencil=4;
+// let erasor=5;
+
+// let output=`Total price is ${pencil+erasor} rupees.`;
+// console.log(output);
+
+let a=5;
+let b=4;
+
+//1.Arithmetic operators.
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+//2.Unary operator:-
+// console.log(a++);//5
+// console.log(++a);//7
+
+//3.Assignment operator:-
+// console.log(a=b);
+// console.log(a+=b);
+// console.log(a-=b);
+// console.log(a*=b);
+// console.log(a/=b);
+// console.log(a%=b);
+
+//4.comparison opertor:-
+// console.log(a<b);
+// console.log(a>b);
+// console.log(a<=b);
+// console.log(a>=b);
+// console.log(a==b);
+// console.log(a!=b);
+
+
+//logical opertor:-
+// console.log(a<=b && a==b);
+// console.log(a<=b || a==b);
+// console.log(!(a<b));
+
+
+
+
+
+//if-condition
+// let age=23;
+// if(age>=18)
+//   {
+//     console.log("You can vote.");
+//   }
+//   if(age<=18)
+//     {
+//        console.log("You can not vote.");
+//     }
+ 
+//else-if condition:-
+// let marks=86;
+// if(marks>=80)
+//   {
+//     console.log("A+");
+//   }
+//   else if(marks>=60)
+//     {
+//       console.log("B+");
+//     }
+//     else if(marks>=40)
+//       {
+//         console.log("C+");
+//       }
+//       else
+//       {
+//         console.log("Fail");
+//       }
+
+//nested-if condition.
+
+// let marks=45;
+// if(marks>=30)
+//   {
+//     console.log("Pass");
+//     if(marks>=80)
+//       {
+//         console.log("great");
+//       }
+//       else
+//       {
+//         console.log("Grade: A")
+//       }
+//   }
+//   else{
+//     console.log("Fail");
+//   }
+
+//switch statement:-
+let day=4;
+switch(day)
+{
+  case 1:
+    {
+       console.log("sunday");break;
+    }
+    case 2:
+      {
+         console.log("Monday");break;
+      }
+      case 3:
+        {
+           console.log("Tuesday");break;
+        }
+        case 4:
+          {
+             console.log("Wednesday");break;
+          }
+          case 5:
+            {
+               console.log("Thrusday");break;
+            }
+            case 6:
+              {
+                 console.log("Friday");break;
+              }
+              case 7:
+                {
+                   console.log("Saturday");break;
+                }
+                default:
+                  {
+                    console.log("None");
+                  }
+    
+}
