ce38b86dfc269770f891cdc5970aa0145e1a850f
Ratnesh Kumar <154334703+ratnesh-45@users.noreply.github.com>
2024-06-24 11:42:40 +0530

    Add files via upload

---
+++ 4. Javascript (part-5) Qs/index.js
@@ -0,0 +1,26 @@
+//Qs.1
+let num=Math.floor(Math.random()*6)+1;
+console.log(num);
+
+
+//Qs.2.
+const car={
+name:"toyota",
+model:"toyo",
+color:"pink"
+};
+
+console.log(car);
+console.log(car.name);
+
+//Qs.3.
+const person={
+name:"ratnesh",
+age:21,
+city:"patna"
+};
+// console.log(person);
+console.log(person.city="new york");
+// console.log(person);
+console.log(person.country="u.s");
+console.log(person);
